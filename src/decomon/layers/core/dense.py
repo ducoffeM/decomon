a9f2c7e96fa5086b6af7278f9d8ae6ffc0f502c8
ducoffeM <melanie.ducoffe@airbus.com>
2024-04-16 11:59:34 +0200

    optimisation of tensor multid input for dense

--- src/decomon/layers/core/dense.py
+++ src/decomon/layers/core/dense.py
@@ -14,6 +14,16 @@ class DecomonDense(DecomonLayer):
         b = self.layer.bias if self.layer.use_bias else K.zeros((self.layer.units,))
 
         # manage tensor-multid input
+        if len(self.layer.input.shape) > 2:
+            shape = list(self.layer.input.shape[1:])
+            dim = K.prod(self.layer.input.shape[1:])
+            input_w = K.reshape(K.identity(dim), [1] + shape + shape)
+            w = K.matmul(input_w, w)[0]
+            shape_b = list(self.layer.input.shape[1:-1])
+            dim_b = K.prod(shape_b)
+            b = K.reshape(K.repeat(b[None], dim_b, axis=0), shape_b + [-1])
+
+        """
         for dim in self.layer.input.shape[-2:0:-1]:
             # Construct a multid-tensor diagonal by blocks
             reshaped_outer_shape = (dim, dim) + w.shape
@@ -26,5 +36,6 @@ class DecomonDense(DecomonLayer):
             w = K.transpose(K.reshape(K.outer(K.identity(dim), w), reshaped_outer_shape), transposed_outer_axes)
             # repeat bias along first dimensions
             b = K.repeat(b[None], dim, axis=0)
+        """
 
         return w, b
